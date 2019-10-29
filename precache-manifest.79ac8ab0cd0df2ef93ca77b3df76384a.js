2c3bf688d7cc23927b5efc610695a87c77f9ccea
Akshay.lr <akshay.lr@tadigital.com>
2019-10-29 18:08:21 +0530

    Updates

---
+++ precache-manifest.79ac8ab0cd0df2ef93ca77b3df76384a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f86ac32dd0c020583e17d61678065c",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "5d48bbb3e7499f55aaca",
+    "url": "/youtube-clone/static/css/main.a5b5cb57.chunk.css"
+  },
+  {
+    "revision": "1c304b888fac81f3073e",
+    "url": "/youtube-clone/static/js/2.81ddde2d.chunk.js"
+  },
+  {
+    "revision": "5d48bbb3e7499f55aaca",
+    "url": "/youtube-clone/static/js/main.cebd1ea8.chunk.js"
+  },
+  {
+    "revision": "746bd68b94edb6cc9b92",
+    "url": "/youtube-clone/static/js/runtime-main.7fdc7e5f.js"
+  }
+]);
